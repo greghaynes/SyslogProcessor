e381a2da29bb8f8b65b9413514db6542a9c968bf
Gregory Haynes <greg@greghaynes.net>
2012-12-10 12:01:20 -0800

    Bumped version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='SyslogProcessor',
-    version='0.1.0',
+    version='0.1.1',
     description='Syslog processing framework',
     author='Gregory Haynes',
     author_email='greg@greghaynes.net',
